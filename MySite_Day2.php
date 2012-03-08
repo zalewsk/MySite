ee61a9484706aab9f0e1cbe43975ad963326cdc5
Michael Zalewski <michaelazalewski@gmail.com>
2012-03-07 22:23:00 -0500

    Began creating the basic structure of my personal site.

---
+++ MySite_Day2.php
@@ -0,0 +1,22 @@
+<html>
+ <head>
+  <title>Mike Zalewski</title>
+ </head>
+ <body>
+ 
+	 <h1><?php echo '<center><p>Mike Zalewski</p></center>'; ?> </h1>
+	 <h2><?php echo '<center><p>Bachelor of Computing Science <i>(Honours, Biomedical Computing)</i></p></center>'; ?> </h2>
+	 <h3><?php echo '<p>Technical Skills</p>'; ?> </h3>
+		I've acquired the following knowledge while completing my degree:
+		<ul>
+		<li> Computer Science </li>
+		<p>Search and sort algorithms (hearistic, greedy, etc.), data structures such as trees and graphs</p>
+		<li> Mathematics & Statistics
+				
+		<li> 
+		</ul>
+	 <h3><?php echo '<p>Leadership Experience</p>'; ?> </h3>
+	 <h3><?php echo '<p>Communicative Abilities</p>'; ?> </h3>
+		The efficiency with which I communicate is the product of IBM training as a Technical Writer and the editing lessons acquired during work on the <a href="2011DepIndex.pdf">2011 Deprivation Index for Kingston, Ontario, Canada</a>.
+ </body>
+</html>
